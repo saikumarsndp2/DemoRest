ccdeaed9666bad9e5115e5aca771c57d54ed7b51
saisirisilla <saisirisilla@LAPTOP-Q8MHA09J>
2025-10-25 10:20:57 +0530

    first commit

---
+++ DemoRest/src/main/java/com/example/DemoRest/DemoController/DController.java
@@ -0,0 +1,17 @@
+package com.example.DemoRest.DemoController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		
+		return "hello";
+	}
+	
+}
